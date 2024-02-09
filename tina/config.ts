bbfa0bc93c114b7e9202aa22a004b1f9c829c7ef
yuto343 <namawasabi1229@gmail.com>
2024-02-09 15:58:40 +0900

    tinacms-2

---
+++ tina/config.ts
@@ -0,0 +1,97 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "/assets",
+      publicFolder: "public",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "_posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            required: true,
+            isTitle: true,
+          },
+          {
+            type: "string",
+            name: "excerpt",
+            label: "Excerpt",
+          },
+          {
+            type: "image",
+            name: "coverImage",
+            label: "CoverImage",
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+          },
+          {
+            type: "object",
+            name: "author",
+            label: "Author",
+            fields: [
+              {
+                type: "string",
+                name: "name",
+                label: "Name",
+              },
+              {
+                type: "image",
+                name: "picture",
+                label: "Picture",
+              },
+            ],
+          },
+          {
+            type: "object",
+            name: "ogImage",
+            label: "OG Image",
+            fields: [
+              {
+                type: "image",
+                name: "url",
+                label: "URL",
+              },
+            ],
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
