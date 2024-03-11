d814a453bbd9ebf40bf10362099022aefbd88bea
SOTOMURA Natsuko <sotomura@pxgrid.com>
2024-03-11 10:59:43 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,10 @@ import MoreStories from "../components/more-stories";
 import HeroPost from "../components/hero-post";
 import Intro from "../components/intro";
 import Layout from "../components/layout";
-import { getAllPosts } from "../lib/api";
 import Head from "next/head";
 import { CMS_NAME } from "../lib/constants";
 import Post from "../interfaces/post";
+import client from "../tina/__generated__/client";
 
 type Props = {
   allPosts: Post[];
@@ -41,14 +41,17 @@ export default function Index({ allPosts }: Props) {
 }
 
 export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
-    "title",
-    "date",
-    "slug",
-    "author",
-    "coverImage",
-    "excerpt",
-  ]);
+  const allPostsData = await client.queries.postConnection();
+  const allPosts = allPostsData.data.postConnection.edges.map((edge) => {
+    return {
+      title: edge.node.title,
+      date: edge.node.date,
+      slug: edge.node._sys.filename,
+      author: edge.node.author,
+      coverImage: edge.node.coverImage,
+      excerpt: edge.node.excerpt,
+    };
+  });
 
   return {
     props: { allPosts },
